6872d3e553a6657cfc8132fd4224a192a995f9d8
Doven Tang <MTDovent@gmail.com>
2024-10-12 03:13:08 +0000

    update compare generate

--- workspace/compare/generate_only_vae.py
+++ workspace/compare/generate_only_vae.py
@@ -1,17 +1,21 @@
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-os.chdir(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+import sys, os, json
+root = os.sep + os.sep.join(__file__.split(os.sep)[1:__file__.split(os.sep).index("Recurrent-Parameter-Generation")+1])
+sys.path.append(root)
+os.chdir(root)
 
 # torch
 import torch
 # father
-from workspace.compare import pdiff_resnet18bn_vae as item
+import importlib
+item = importlib.import_module(f"{sys.argv[1]}")
 Dataset = item.Dataset
 train_set = item.train_set
 config = item.config
 model = item.model
 vae = item.vae
-config["tag"] = config.get("tag") if config.get("tag") is not None else os.path.basename(item.__file__)[:-3]
+assert config.get("tag") is not None, "Remember to set a tag."
+
+
 
 
 generate_config = {
@@ -23,6 +27,7 @@ generate_config = {
     "need_test": True,
 }
 config.update(generate_config)
+
 
 
 
@@ -40,7 +45,7 @@ print('==> Defining generate..')
 def generate(save_path=config["generated_path"], test_command=config["test_command"], need_test=True):
     print("\n==> Generating..")
     model.eval()
-    with torch.cuda.amp.autocast(False, torch.bfloat16):
+    with torch.cuda.amp.autocast(True, torch.bfloat16):
         with torch.no_grad():
             mu = model(sample=True)
             mu = torch.randn_like(mu)
@@ -50,7 +55,8 @@ def generate(save_path=config["generated_path"], test_command=config["test_comma
     train_set.save_params(prediction, save_path=save_path)
     if need_test:
         os.system(test_command)
-        print("\n")
+
+
 
 
 if __name__ == "__main__":
--- workspace/compare/generate_with_vae.py
+++ workspace/compare/generate_with_vae.py
@@ -1,17 +1,21 @@
-import sys, os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
-os.chdir(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
+import sys, os, json
+root = os.sep + os.sep.join(__file__.split(os.sep)[1:__file__.split(os.sep).index("Recurrent-Parameter-Generation")+1])
+sys.path.append(root)
+os.chdir(root)
 
 # torch
 import torch
 # father
-from workspace.compare import pdiff_resnet18bn_vae as item
+import importlib
+item = importlib.import_module(f"{sys.argv[1]}")
 Dataset = item.Dataset
 train_set = item.train_set
 config = item.config
 model = item.model
 vae = item.vae
-config["tag"] = config.get("tag") if config.get("tag") is not None else os.path.basename(item.__file__)[:-3]
+assert config.get("tag") is not None, "Remember to set a tag."
+
+
 
 
 generate_config = {
@@ -20,9 +24,10 @@ generate_config = {
     "checkpoint": f"./checkpoint/{config['tag']}.pth",
     "generated_path": os.path.join(Dataset.generated_path.rsplit("/", 1)[0], "generated_{}_{}.pth"),
     "test_command": os.path.join(Dataset.test_command.rsplit("/", 1)[0], "generated_{}_{}.pth"),
-    "need_test": False,
+    "need_test": True,
 }
 config.update(generate_config)
+
 
 
 
@@ -40,7 +45,7 @@ print('==> Defining generate..')
 def generate(save_path=config["generated_path"], test_command=config["test_command"], need_test=True):
     print("\n==> Generating..")
     model.eval()
-    with torch.cuda.amp.autocast(False, torch.bfloat16):
+    with torch.cuda.amp.autocast(True, torch.bfloat16):
         with torch.no_grad():
             mu = model(sample=True)
             prediction = vae.decode(mu)
@@ -49,7 +54,8 @@ def generate(save_path=config["generated_path"], test_command=config["test_comma
     train_set.save_params(prediction, save_path=save_path)
     if need_test:
         os.system(test_command)
-        print("\n")
+
+
 
 
 if __name__ == "__main__":
