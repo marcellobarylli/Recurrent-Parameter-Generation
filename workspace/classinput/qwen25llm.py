7cccaa0755b1e770aa74b3d66664e00db170288b
kaiwang960112 <kai.wang960112@gmail.com>
2025-01-18 16:16:48 +0800

    update fixed qwen name.

--- workspace/classinput/qwen25llm.py
+++ workspace/classinput/qwen25llm.py
@@ -7,7 +7,7 @@ model_name = os.path.join(os.path.dirname(__file__), "Qwen25llm")
 
 print("Downloading Qwen2.5 files...")
 from huggingface_hub import snapshot_download
-snapshot_download(repo_id="Qwen2.5-3B-Instruct",
+snapshot_download(repo_id="Qwen/Qwen2.5-3B-Instruct",
                   repo_type="model",
                   cache_dir=model_name,
                   local_dir_use_symlinks=False,
